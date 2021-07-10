ea261bbf7636c01752dd0b261a19c2474989ad58
Deszcelo <rvallejosbarria@gmail.com>
2021-07-10 17:25:28 -0400

    Creating Objects with the Factory Pattern

---
+++ src/main/java/factory/Food.java
@@ -0,0 +1,45 @@
+package main.java.factory;
+
+public abstract class Food {
+    private int quantity;
+    public Food(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public abstract void consumed();
+}
+
+class Hay extends Food {
+    public Hay(int quantity) {
+        super(quantity);
+    }
+
+    @Override
+    public void consumed() {
+        System.out.println("Hay eaten: " + getQuantity());
+    }
+}
+
+class Pellets extends Food {
+    public Pellets(int quantity) {
+        super(quantity);
+    }
+
+    @Override
+    public void consumed() {
+        System.out.println("Pellets eaten: " + getQuantity());
+    }
+}
+
+class Fish extends Food {
+    public Fish(int quantity) {
+        super(quantity);
+    }
+
+    @Override
+    public void consumed() {
+        System.out.println("Fish eaten: " + getQuantity());
+    }
+}
